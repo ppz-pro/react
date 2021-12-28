2b21b4ef75d104585d79dd28ba425e4d8f158b59
gy <372301467@qq.com>
2021-12-28 18:00:46 +0800

    update

--- src/cmp/button.jsx
+++ src/cmp/button.jsx
@@ -4,11 +4,18 @@ import '@ppzp/style/button.css'
 
 export default function() {
   return <Block title = 'Button' className = 'btns'>
-    <button className = 'btn'>普通</button>
-    <button className = 'btn strong'>strong</button>
-    <button className = 'btn danger'>危险</button>
-    <button className = 'btn danger strong'>strong</button>
-    <a className = 'btn'>链接</a>
-    <a className = 'btn danger'>危险链接</a>
+    <button className = 'btn'>button.btn</button>
+    <button className = 'btn danger'>button.btn.danger</button>
+    <br/>
+    <button className = 'btn strong'>button.btn.strong</button>
+    <button className = 'btn danger strong'>button.btn.danger.strong</button>
+    <br/>
+    <button className = 'btn link'>button.btn.link</button>
+    <button className = 'btn link danger'>button.btn.link.danger</button>
+    <br/>
+    <a className = 'btn'>a.btn</a>
+    <a className = 'btn danger'>a.btn.danger</a>
+    <br/>
+    <a className = 'btn' href = 'https://github.com/ppz-pro/react'>github</a>
   </Block>
 }
--- src/cmp/hr.jsx
+++ src/cmp/hr.jsx
@@ -4,8 +4,8 @@ import '@ppzp/style/hr.css'
 
 export default function() {
   return <Block title = "分割线">
-    一段文字...
+    <p><strong><code>@ppzp/react</code></strong> 是一个 <a className = 'btn' href = 'https://reactjs.org/'>react</a> 组件库。简洁、简单、实用。</p>
     <div className = 'hr'>分割线</div>
-    另一段文字...
+    <p>编程本来很好玩，直到你把它写得繁琐。</p>
   </Block>
 }
