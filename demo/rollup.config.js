58faac1bac0996c445f3464286884392c5363330
关于 <372301467@qq.com>
2022-01-10 10:31:30 +0800

    支持 rollup-plugin-serve

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -3,15 +3,15 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import babel from '@rollup/plugin-babel'
-
-// dev server:
-// https://blog.bitsrc.io/trying-rollup-for-react-applications-d3c2304d16bf
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 
 export default {
   input: 'demo/index.js',
   output: {
     file: 'demo/index.dist.js',
-    format: 'iife'
+    format: 'iife',
+    sourcemap: true
   },
   plugins: [
     babel(),
@@ -24,6 +24,10 @@ export default {
       stylus: {
         paths: [`${process.cwd()}/node_modules`]
       }
+    }),
+    serve('demo'),
+    livereload({
+      watch: 'demo'
     })
   ]
 }
